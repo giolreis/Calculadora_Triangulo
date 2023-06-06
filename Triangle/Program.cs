51cd0e732a1e2d8d2e0fa4af0b290cad9ded6aa1
alissonbastos <alissonpaulo@outlook.com.br>
2023-06-05 21:18:14 -0300

    V3.2

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -14,7 +14,7 @@ namespace Triangle
 
             Console.WriteLine("Bem vindo ao Calc \n\n");
             Console.WriteLine("Calcule a area do triangulo \n\n");
-            Console.WriteLine("");
+            Console.WriteLine("Calculando área do triângulo"); //V3.2
             a = double.Parse(Console.ReadLine());
         }
     }
