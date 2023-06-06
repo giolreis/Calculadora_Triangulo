fb3317f7d446918f16ca244680978dfe055be21b
giolreis <giovaniluizreis@gmail.com>
2023-06-05 21:02:01 -0300

    Adicionar arquivos de projeto.

---
+++ Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b c;
+
+            Console.WriteLine("Bem vindo ao Calc \n\n");
+            Console.WriteLine("Calcule a area do triangulo");
+            a = double.Parse(Console.ReadLine());
+        }
+    }
+}
